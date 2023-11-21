c21500ce004cd0b8cb4cac3045c75669b9bbef96
tommycbird <tommycbird@gmail.com>
2023-11-21 14:20:21 -0500

    init

---
+++ artplacement.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// Takes in a mesh and returns a random point within bounds
+void randomPoint(){
+    
+}
+
+
+int main(){
+    int numpoints = 100;
+
+    // CONVERT 1D WALLS TO 2D WALLS
+
+    // TRIANGULATE THIS MESH
+
+    // RANDOMLY PLACE X POINTS
+
+    // FOR EACH POINT, DETERMINE WHICH EDGES ARE VISIBLE
+
+    // VISUALIZE WALLS WITH HEATMAP COLORS
+
+}
